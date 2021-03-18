79de5b81de1cf682a4fe89badffe99b8d4e20706
Or <or@h-MacBook-Pro-sl-Or.local>
2021-03-19 00:51:23 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Or Minzberg'));
+app.get('/', (req, res) => res.send('It is getting late we should go to sleep Zzzzz..Zzzz..'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
